eb13969e5964d107f8a560c9eed11879a66cd9f3
basupatil1213 <patil.ba@northeastern.edu>
2024-07-16 14:59:23 -0400

    Day4: Loops

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 import { day1_learning } from "./Day1.js";
 import { day2_learning } from "./Day2.js";
 import { day3_learning } from "./Day3.js";
+import { day4_learning } from "./Day4.js";
 
 export const formatter = "*".repeat(5) + " " + "-".repeat(10) + " " + "*".repeat(5);
 
-// Day1: Variables and Data Types
-console.log(`\n\n${formatter} Day1: Variables and Data Types ${formatter}\n\n`);
-day1_learning();
+// // Day1: Variables and Data Types
+// console.log(`\n\n${formatter} Day1: Variables and Data Types ${formatter}\n\n`);
+// day1_learning();
 
-// Day2: Operators
-console.log(`\n\n${formatter} Day2: Operators ${formatter}\n\n`);
-day2_learning();
+// // Day2: Operators
+// console.log(`\n\n${formatter} Day2: Operators ${formatter}\n\n`);
+// day2_learning();
 
-// Daye3: Control Structures
-console.log(`\n\n${formatter} Daye3: Control Structures ${formatter}\n\n`);
-day3_learning();
+// // Daye3: Control Structures
+// console.log(`\n\n${formatter} Day3: Control Structures ${formatter}\n\n`);
+// day3_learning();
+
+// Daye4: Loops
+console.log(`\n\n${formatter} Day4: Loops ${formatter}\n\n`);
+day4_learning();
